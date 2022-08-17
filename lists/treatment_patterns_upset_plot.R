65dc39e48acead9319a8d83bb99f6fd08393eb9b
Max Salvatore <mmsalva@umich.edu>
2022-08-17 11:17:37 -0400

    update upset plot to work on source from run.R

--- lists/treatment_patterns_upset_plot.R
+++ lists/treatment_patterns_upset_plot.R
@@ -1,14 +1,14 @@
 library(UpSetR)
 library(data.table)
 
-d <- as.data.table(readRDS("objects/main_data.rds"))[AnyCancerPhe == 1][, surgery := surgery_dsb]
+d <- as.data.table(readRDS(paste0("data/", cohort_version,"/main_data_", cohort_version, ".rds")))[AnyCancerPhe == 1][, surgery := surgery_dsb]
 
 setnames(d, old = "imt", new = "immunotherapy")
 
 upset_plot <- upset(d, sets = c("radiation", "chemo", "surgery", "immunotherapy"), sets.bar.color = "#56B4E9",
                     order.by = "freq", empty.intersections = "on")
 
-cairo_pdf(filename = "objects/cancer_treatment_upset_plot.pdf", width = 7, height = 5)
+cairo_pdf(filename = paste0("objects/", cohort_version,"/cancer_treatment_upset_plot.pdf"), width = 7, height = 5)
 print(upset_plot)
 dev.off()
 
